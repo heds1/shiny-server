fe916ea9ec298ca0226fd2e619eb7446efd36f2f
heds1 <hedley.stirrat@gmail.com>
2020-06-06 17:49:11 +1200

    feat: projections

--- myfi/app.r
+++ myfi/app.r
@@ -5,106 +5,196 @@
 
 library(shiny)
 library(dplyr)
+library(ggplot2)
 
 ui <- {
 	fluidPage(
-
 		sidebarLayout(
 			sidebarPanel(
 				h2('Summary'),
-				div(
-					p(
-						'Current net worth: '
-					),
-					textOutput(
-						outputId = 'current_nw'
-					)
+				div(style="display: inline-flex;",
+					p(style="padding-right: 0.5rem;", 'Current net worth: '),
+					textOutput(outputId = 'current_nw')
+				),
+				div(style="display: inline-flex;",
+					p(style="padding-right: 0.5rem;", 'FI number: '),
+					textOutput(outputId = 'fi_number_sidebar')
 				),
 				div(
-					p(
-						'FI number: '
-					),
-					textOutput(
-						outputId = 'fi_num'
-					)
-				),
-				div(
-					p(
-						'% progress toward financial independence'
-					),
-					uiOutput(
-						'fi_progress'
-					)
-					
+					textOutput('fi_progress_text'),
+					uiOutput('fi_progress_bar')	
 				)
 				
 			),
 			mainPanel(
-			tabsetPanel(
-				tabPanel(
-					'Your numbers',
+				tabsetPanel(
+					tabPanel(
+						'Your FI numbers',
+						p('Calculate your FI number here'),
+						numericInput(
+							inputId = 'annual_withdrawal',
+							label = 'What is your target annual income from retirement accounts?',
+							value = 50000
+						),
+						numericInput(
+							inputId = 'swr',
+							label = 'What is your target safe withdrawal rate?',
+							value = 0.035
+						),
 						numericInput(
 							inputId = 'current_nw',
 							label = 'Current net worth',
-							value = 0
+							value = 40000
 						),
 						numericInput(
-							inputId = 'fi_num',
-							label = 'FI number',
-							value = 1000000
+							inputId = 'num_properties',
+							label = 'Current number of properties',
+							value = 0
+						),
+						uiOutput(
+							'properties'
 						)
-				),
-				tabPanel(
-					'Projections',
-					p('test')
-				),
-				tabPanel(
-					'Plots'
+					),
+					tabPanel(
+						'Projections',
+						numericInput(
+							inputId = 'yearly_savings',
+							label = 'How much will you save per year for retirement?',
+							value = 20000
+						),
+						numericInput(
+							inputId = 'interest_rate',
+							label = 'What is the expected % return on your investments?',
+							value = 5
+						),
+						textOutput(
+							'years_to_retirement_text'
+						),
+						plotOutput(
+							'years_to_retirement_plot'
+						)
+					)
 				)
 			)
-		)
-
-		)
-
-		
-		
+		)	
 	)
+}
+
+# compound_interest <- function(rate, years, initial, injection) {
+# 	# rate = interest rate as decimal (p.a.)
+# 	# years = number of years to compound
+# 	# initial = initial investment
+# 	# injection = extra savings each year
+# 	total <- initial
+
+# 	for (year in 1:years) {
+
+# 		total <- total + rate * total + injection
+	
+# 	}
+
+# 	return (total)
+
+# }
+
+# function to calculate number of years until retirement
+cmpd_interest_until_target <- function(rate, initial, target, injection) {
+	dat <- data.frame(year = 0, principal = initial)
+	while(dat[nrow(dat), 'principal'] < target) {
+		this_year <- dat[nrow(dat), 'year'] + 1
+		this_principal <- dat[nrow(dat), 'principal'] + rate * dat[nrow(dat), 'principal'] + injection
+		dat <- bind_rows(dat, data.frame(year = this_year, principal = this_principal))
+	}
+	return (dat)
 }
 
 server <- function(input, output, session) {
 
-	output$current_nw <- reactive({
-		input$current_nw
+	# calculate FI number
+	fi_number <- reactive({
+		this_num <- round(input$annual_withdrawal / input$swr, 0)
+		digits <- nchar(this_num)/2
+		signif(this_num, digits = digits)
 	})
 
-	output$fi_num <- reactive({
-		input$fi_num
+	# render FI number in sidebar
+	output$fi_number_sidebar <- renderText({
+		paste0("$", fi_number())
 	})
 
-	output$fi_progress <- renderUI({
+	# convert interest rate to percentage if required
+	# naive, just assumes that if a rate is above 20% they mean the % rather
+	# than actual value
+	cleaned_interest_rate <- reactive({
+		if (input$interest_rate > .2) {
+			return (input$interest_rate / 100)
+		} else {
+			return (input$interest_rate)
+		}
+	})
 
-		# get percent FI based on inputs
-		percent_fi <- input$current_nw / input$fi_num * 100
+	# calculate years to retirement, return list containing years and plot
+	years_to_retirement <- reactive({
+		df <- cmpd_interest_until_target(rate = cleaned_interest_rate(), initial = input$current_nw, target = fi_number(), injection = input$yearly_savings)
+		years <- df[nrow(df), 'year']
+		plt <- ggplot(df, aes(x = year, y = principal)) + geom_point()
+		return(list(years=years, plot = plt))
+	})
+
+	# output text of years to retirement
+	output$years_to_retirement_text <- renderText({
+		paste0("With the above settings, you will be able to retire in ", years_to_retirement()$years, " years!")
+	}) 
+
+	# output plot of years to retirement
+	output$years_to_retirement_plot <- renderPlot({
+		years_to_retirement()$plot
+	})
+
+	# render current net worth
+	output$current_nw <- renderText({
+		paste0(" $", input$current_nw)
+	})
+
+	# reactive percent FI
+	percent_fi <- reactive({
+		round(input$current_nw / fi_number() * 100, 1)
+	})
+
+	# render FI progress text
+	output$fi_progress_text <- renderText({
+		paste0("You are ", percent_fi(), "% financially independent.")
+	})
+
+	# render bootstrap progress bar
+	output$fi_progress_bar <- renderUI({
 
 		# generate HTML for progress bar
 		HTML(
 			paste0(
-				"
-					<div class=\"progress\">
-						<div class=\"progress-bar\" role=\"progressbar\" style=\"width: ", percent_fi, "%\" aria-valuenow=\"", percent_fi, "\" aria-valuemax=\"100\" 
-					</div>
-					
-				"
-				
+				"<div class=\"progress\">
+					<div class=\"progress-bar\" role=\"progressbar\" style=\"width: ", percent_fi(), "%\" aria-valuenow=\"", percent_fi(), "\" aria-valuemax=\"100\" 
+				 </div>"
 			)
-			# '
-			# <div class="progress">
-			# 	<div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-			# </div>
-			# '
 		)
-		# todo add
+	})
 
+	output$properties <- renderUI({
+		num_properties <- input$num_properties
+
+		if (num_properties > 0) {
+			
+			lapply(1:num_properties, function(i) {
+				numericInput(
+					inputId = paste0("ind", i),
+					label = paste0("Value of property ", i),
+					value = 0,
+					step = 20000
+				)
+			}) 
+		}
+
+		
 	})
 
 }
@@ -113,22 +203,7 @@ shinyApp(ui, server)
 
 
 
-compound_interest <- function(rate, years, initial, injection) {
-	# rate = interest rate as decimal (p.a.)
-	# years = number of years to compound
-	# initial = initial investment
-	# injection = extra savings each year
-	total <- initial
 
-	for (year in 1:years) {
-
-		total <- total + rate * total + injection
-	
-	}
-
-	return (total)
-
-}
 
 
 # # assume
